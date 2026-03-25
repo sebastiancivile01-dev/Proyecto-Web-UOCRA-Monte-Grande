62bb5788b4c24e47a0e472b84b299aae7f0cb6bd
sebastiancivile01-dev <sebastian.civile01@gmail.com>
2026-03-25 19:39:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ with st.sidebar:
                 <a href="https://www.uocra.org/" target="_blank" style="text-decoration: none; color: #0033A0; font-weight: bold;">UOCRA</a> &nbsp;|&nbsp;   
                 <a href="https://cgtoficial.org/" target="_blank" style="text-decoration: none; color: #0033A0; font-weight: bold;">CGT</a> &nbsp;|&nbsp;                  
                 <a href="https://mutual.uocra.org/" target="_blank" style="text-decoration: none; color: #0033A0; font-weight: bold;">MUTUAL</a> &nbsp;|&nbsp;                      
-                <a href="https://www.infoleg.gob.ar/" target="_blank" style="text-decoration: none; color: #0033A0; font-weight: bold;">INFOLEG</a> &nbsp;|&nbsp; 
+                <a href="https://servicios.infoleg.gob.ar/infolegInternet/anexos/25000-29999/27238/norma.htm" target="_blank" style="text-decoration: none; color: #0033A0; font-weight: bold;">INFOLEG</a> &nbsp;|&nbsp; 
             </div>
         </div>
         <hr style="margin-top: 10px; margin-bottom: 15px;">
