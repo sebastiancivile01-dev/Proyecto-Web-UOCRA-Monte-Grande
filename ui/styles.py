6ffb6c3d4b0cbc3f1fc75f7bf82fc6ec8a65e4ea
sebastiancivile01-dev <sebastian.civile01@gmail.com>
2026-07-23 12:06:07 -0300

    Create styles.py

---
+++ ui/styles.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def aplicar_estilos_globales():
+    st.markdown("""
+        <style>
+
+        /* ACÁ PEGÁS TODO EL CSS GLOBAL ACTUAL */
+
+        </style>
+    """, unsafe_allow_html=True)
